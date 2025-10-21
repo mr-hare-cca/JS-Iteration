9ab22632e300f9d4b8aa76bd57801c490c660d6f
mr-hare-cca <150952432+mr-hare-cca@users.noreply.github.com>
2025-10-21 21:33:37 +0000

    js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-// add javascript here
-var age;
+function calculate() {
+  const age = Number(document.getElementById("age").value);
+  let price;
+
+  if (age <= 12) {
+    price = 7;
+  } else if (age <= 17) {
+    price = 12;
+  } else if (age <= 64) {
+    price = 20;
+  } else {
+    price = 12;
+  }
+
+  document.getElementById("priceOutput").textContent = "$" + price;
+}
